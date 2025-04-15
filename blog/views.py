1721f23b23b8ad5266c51831ed430f58b42d544c
vacuum <wsjj331@gmail.com>
2025-04-15 16:00:14 +0900

    DetailView 클래스 추가

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from . models import Post
 
 class PostList(ListView):
     model = Post
     ordering = '-pk'
+
+class PostDetail(DetailView):
+    model = Post
+    # template_name = 'blog\single_post_page.html'
     # template_name = 'blog\index.html'
 # def index(request):
 #     posts = Post.objects.all().order_by('-pk')
